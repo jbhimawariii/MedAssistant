71b68d1e1b2d7c8b1a55913dcfdc5baab7151579
krlm0 <krlm0@protonmail.com>
2021-12-20 21:01:25 +0800

    Revert "minimal branch"
    This reverts commit 0ccedd58fce8909ffb01380c2a72d8fb21a4760e.

--- medassistant.py
+++ medassistant.py
@@ -20,7 +20,7 @@ from aiy.assistant.library import Assistant
 
 
 # The class implementation could be somewhat better..
-class medAssistant:
+class medicalAssistant:
     def __init__(self):
         self._task = threading.Thread(target=self.runTask)
         self._medAssistant = None
@@ -131,10 +131,104 @@ class medAssistant:
             tts.say("Goodbye")
             sys.exit(0)
 
+    def textInput(self, input):
+        if self._startConvo:
+            self._medAssistant.send_text_query(input)
+
+
+# huh.
+class medGui:
+    def __init__(self):
+        self._root = tk.Tk()
+        self._root.geometry("400x450")
+        self._root.resizable(False, False)
+        self._root.configure(background="#141316")
+
+        self._topFrame = ttk.Frame(self._root, padding="20 20 22 22", width=350, height=400)
+        self._topFrame.pack()
+        self._topFrame.pack_propagate(False)
+        self._topFrame.place(relx=.5, rely=.5, anchor="center")
+
+        self._icon = tk.PhotoImage(file="assets/githubicon.png")
+
+        self._assistant = medicalAssistant()
+        self._input = tk.StringVar()
+
+    def openCredits(self):
+        # TODO improvements
+        subprocess.run("chromium https://github.com/JairusBGit/MedAssistant", shell=True)
+
+    def addProfile(self):
+        filename = filedialog.askopenfilename()
+        # did you know that i have spent 3 hours on this thing because of some bug i cant find?
+        # apparently the fix was removing the window title
+        # huh.
+        name = sd.askstring("Input Name", "Enter Patient's name")
+        if name is None:  # if no input has been made
+            return
+
+        # format name
+        name = name.lower()
+        name = name.replace(" ", "")
+        name = "profiles/" + name + ".pdf"
+
+        copyfile(filename, name)
+        self._assistant.refreshIndex()
+
+    def textCommand(self):
+        command = sd.askstring("Input Command", "Enter Command")
+        if command is None:
+            return
+        else:
+            self._assistant.textInput(command)
+
+    # bruh
+    def addStyle(self):
+        teststyle = ttk.Style()
+
+        teststyle.configure("TFrame", background="#1b1c1f")
+        teststyle.configure("TButton", font=("Lato", 18), borderwidth=0, background="#1b1c1f",
+                            foreground="#c9c2bd", activeforeground="#ffffff",
+                            activebackground="#1b1c1f")
+        teststyle.configure("TSeparator", background="#ffffff")
+        teststyle.configure("header.TLabel", font=("Lato", 24), background="#1b1c1f",
+                            foreground="#c9c2bd")
+        teststyle.configure("normal.TLabel", font=("Lato", 10), background="#1b1c1f",
+                            foreground="#c9c2bd")
+        teststyle.configure("TEntry", borderwidth=0, fieldbackground="#1b1c1f", foreground="#c9c2bd",
+                            relief="flat", justify="center")
+
+    # note to self, avoid writing gui programs
+    def makeWidgets(self):
+        ttk.Label(self._topFrame, text="MedAssistant", style="header.TLabel").pack(side=tk.TOP,
+                                                                                   anchor=tk.NW)
+        ttk.Separator(self._topFrame, orient=tk.VERTICAL).pack(side=tk.LEFT, fill="y",
+                                                               padx=20)
+        # AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+        ttk.Button(self._topFrame, text="Activate", command=self._assistant.buttonPressed).place(x=40, y=100)
+        ttk.Button(self._topFrame, text="+", command=self.addProfile).place(x=120, y=200)
+        ttk.Button(self._topFrame, text="Refresh", command=self._assistant.refreshIndex).place(x=40, y=200)
+        ttk.Button(self._topFrame, text="Manual Input", command=self.textCommand).place(x=60, y=300)
+
+        self._root.bind("<Return>",
+                        lambda event, var=input:
+                            self._assistant.textInput(var))
+
+        ttk.Label(self._topFrame, text="Ver 0.9.4", style="normal.TLabel").place(x=240, y=360)
+
+        ttk.Button(self._topFrame, image=self._icon, command=self.openCredits).place(x=300, y=350)
+
+    def start(self):
+        self._assistant.startAssistant()
+        self.addStyle()
+        self.makeWidgets()
+        self._root.mainloop()
+
+
 def main():
     logging.basicConfig(level=logging.INFO)
-    assist = medAssistant()
-    assist.start()
+    gui = medGui()
+    gui.start()
 
 
 if __name__ == "__main__":
